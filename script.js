409a8b5b8ae3993c4afe5559e0f68dd58c9936f3
Aryan Singh <87224341+aryannsingh@users.noreply.github.com>
2021-12-22 22:00:45 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,94 @@
+function openNav() {
+  document.getElementById("mySidebar").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidebar").style.width = "0";
+  document.getElementById("main").style.marginLeft= "0";
+}
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
+    document.getElementById("nav").style.backgroundColor = "black";
+    document.getElementById("a1").style.color = "white";
+    document.getElementById("a2").style.color = "white";
+    document.getElementById("a3").style.color = "white";
+    document.getElementById("a4").style.color = "white";
+  } else {
+    document.getElementById("nav").style.background = "#fff";
+    document.getElementById("a1").style.color = "#323232";
+    document.getElementById("a2").style.color = "#323232";
+    document.getElementById("a3").style.color = "#323232";
+    document.getElementById("a4").style.color = "#323232";
+  }
+
+}
+
+  class TypeWriter {
+    constructor(txtElement, words, wait = 3000) {
+      this.txtElement = txtElement;
+      this.words = words;
+      this.txt = '';
+      this.wordIndex = 0;
+      this.wait = parseInt(wait, 10);
+      this.type();
+      this.isDeleting = false;
+    }
+  
+    type() {
+      // Current index of word
+      const current = this.wordIndex % this.words.length;
+      // Get full text of current word
+      const fullTxt = this.words[current];
+  
+      // Check if deleting
+      if(this.isDeleting) {
+        // Remove char
+        this.txt = fullTxt.substring(0, this.txt.length - 1);
+      } else {
+        // Add char
+        this.txt = fullTxt.substring(0, this.txt.length + 1);
+      }
+  
+      // Insert txt into element
+      this.txtElement.innerHTML = `<span class="txt">${this.txt}</span>`;
+  
+      // Initial Type Speed
+      let typeSpeed = 80;
+  
+      if(this.isDeleting) {
+        typeSpeed /= 2;
+      }
+  
+      // If word is complete
+      if(!this.isDeleting && this.txt === fullTxt) {
+        // Make pause at end
+        typeSpeed = this.wait;
+        // Set delete to true
+        this.isDeleting = true;
+      } else if(this.isDeleting && this.txt === '') {
+        this.isDeleting = false;
+        // Move to next word
+        this.wordIndex++;
+
+        typeSpeed = 500;
+      }
+  
+      setTimeout(() => this.type(), typeSpeed);
+    }
+  }
+  
+  
+  // Init On DOM Load
+  document.addEventListener('DOMContentLoaded', init);
+  
+  // Init App
+  function init() {
+    const txtElement = document.querySelector('.txt-type');
+    const words = JSON.parse(txtElement.getAttribute('data-words'));
+    const wait = txtElement.getAttribute('data-wait');
+    // Init TypeWriter
+    new TypeWriter(txtElement, words, wait);
+  }
